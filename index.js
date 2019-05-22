3604c877b46308356a6040cb1d009c8b542a6133
Thomas Rory Gummerson <thomas@gummerson.no>
2019-05-22 23:20:24 +0200

    Support proxies

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const { lookup } = require('geoip-lite');
 const { stringify } = JSON;
 
 const getIP = req => {
+	const proxied = req.headers['x-forwarded-for'].split(',')[0].trim();
 	const param = req.url.slice(1);
 	const remote = req.connection.remoteAddress;
 	return (
+		isIP(proxied)
+			? proxied :
 		isIP(param)
 			? param :
 		remote.startsWith('::ffff:')
@@ -19,9 +22,14 @@ const getIP = req => {
 };
 
 const server = createServer((req, res) => {
-	return res.end(stringify(lookup(getIP(req)) || (
-		res.writeHead(500),
-		{ error: 'No results' })));
+	const ip = getIP(req);
+	const location = lookup(ip);
+	const ok = Boolean(location);
+	return res.end(stringify(location
+		? { ok, ip, ...location }
+		: (
+			res.writeHead(500),
+			{ ok, ip, error: 'No results!', headers: req.headers })));
 });
 
 server.listen(process.env.PORT);
